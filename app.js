037e9ef722527ac75f5b78b3101b66a55589feae
MD. AMANULLAH <35330230+Md-Aman@users.noreply.github.com>
2019-05-20 22:49:36 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+// Modules
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyparser = require("body-parser");
+var cors = require("cors");
+
+// core modules
+var path = require("path");
+
+/*................................*/
+var app = express();
+
+app.use(cors());
+app.use(bodyparser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res)=>{
+    res.send("its working");
+})
+
+app.listen(3000, ()=>{
+    console.log("server started");
+})
+
